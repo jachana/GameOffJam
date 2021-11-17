62e70d82db3ab2521d4ff977f472864684ddfcd6
Víctor Adrián Suarez Ruiz <jr._5oct1998@hotmail.com>
2021-11-17 13:01:41 -0600

    Add player sprite fliping

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -10,31 +10,52 @@ public class PlayerController : MonoBehaviour
     private Rigidbody2D _rigidbody;
     private bool _is_grounded;
     private SpriteRenderer _sprite_renderer;
-    private float movement_x_direction;
+    private Animator _animator;
+    private float horizontal_direction;
+
+    // Animation keys
+    string is_running_key = "is_running";
 
     void Start()
     {
         _rigidbody = GetComponent<Rigidbody2D>();
         _sprite_renderer = GetComponent<SpriteRenderer>();
+        _animator = GetComponent<Animator>();
     }
 
     void Update()
     {
-        CheckPlayerInput(); 
+        CheckPlayerInput();
+        FlipCharacter();
     }
 
     private void FixedUpdate()
     {
-        _rigidbody.velocity = new Vector2(movement_x_direction * _player_speed, _rigidbody.velocity.y);
+        _rigidbody.velocity = new Vector2(horizontal_direction * _player_speed, _rigidbody.velocity.y);
     }
 
     void CheckPlayerInput()
     {
-        movement_x_direction = Input.GetAxisRaw("Horizontal");
+        horizontal_direction = Input.GetAxisRaw("Horizontal");
+
+        bool is_running = horizontal_direction != 0;
+        _animator.SetBool(is_running_key, is_running);
 
         if (Input.GetButtonDown("Jump") && _is_grounded)
         {
             _rigidbody.AddForce(new Vector2(0, _jump_force), ForceMode2D.Impulse);
+        }
+    }
+
+    void FlipCharacter()
+    {
+        if(horizontal_direction < 0)
+        {
+            _sprite_renderer.flipX = true;
+        }
+        else if(horizontal_direction > 0)
+        {
+            _sprite_renderer.flipX = false;
         }
     }
 
