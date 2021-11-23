57429fd8af1a9b3d5be9ccf64c3b79c3eac2978b
Víctor Adrián Suarez Ruiz <jr._5oct1998@hotmail.com>
2021-11-22 19:03:08 -0600

    Modify Player Controller

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -1,91 +1,241 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField]
-    private float _player_speed = 2;
-    [SerializeField]
-    private float _jump_force = 45;
 
-    private Rigidbody2D _rigidbody;
-    private bool _is_grounded;
+    [SerializeField]
+    private float movementSpeed;
+    [SerializeField]
+    private float groundCheckRadius;
+    [SerializeField]
+    private float jumpForce;
+    [SerializeField]
+    private float slopeCheckDistance;
+    [SerializeField]
+    private float maxSlopeAngle;
+    [SerializeField]
+    private Transform groundCheck;
+    [SerializeField]
+    private LayerMask whatIsGround;
+    [SerializeField]
+    private PhysicsMaterial2D noFriction;
+    [SerializeField]
+    private PhysicsMaterial2D fullFriction;
+
+    private float xInput;
+    [SerializeField] private float slopeDownAngle;
+    private float slopeSideAngle;
+    [SerializeField] private float lastSlopeAngle;
+
+    [SerializeField] private bool isGrounded;
+    [SerializeField] private bool isOnSlope;
+    private bool isJumping;
+    [SerializeField] private bool canWalkOnSlope;
+    private bool canJump;
+
+    private Vector2 newVelocity;
+    private Vector2 newForce;
+    private Vector2 capsuleColliderSize;
+
+    private Vector2 slopeNormalPerp;
+
+    private Rigidbody2D rb;
     private SpriteRenderer _sprite_renderer;
     private Animator _animator;
-    private float horizontal_direction;
+    private CapsuleCollider2D capsuleCollider;
 
     // Animation keys
     string is_running_key = "is_running";
     string is_grounded_key = "is_grounded";
     string y_velocity_key = "y_velocity";
 
-    void Start()
+    private void Start()
     {
-        _rigidbody = GetComponent<Rigidbody2D>();
+        rb = GetComponent<Rigidbody2D>();
         _sprite_renderer = GetComponent<SpriteRenderer>();
         _animator = GetComponent<Animator>();
+        capsuleCollider = GetComponent<CapsuleCollider2D>();
+
+        capsuleColliderSize = capsuleCollider.size;
     }
 
-    void Update()
+    private void Update()
     {
-        CheckPlayerInput();
+        CheckInput();
         UpdateAnimatorKeys();
-        FlipCharacter();
+    }
+    void UpdateAnimatorKeys()
+    {
+        bool is_running = xInput != 0;
+        _animator.SetBool(is_running_key, is_running);
+
+        _animator.SetBool(is_grounded_key, isGrounded);
+        _animator.SetFloat(y_velocity_key, rb.velocity.y);
     }
 
     private void FixedUpdate()
     {
-        if (_is_grounded)
-            _rigidbody.velocity = new Vector2(horizontal_direction * _player_speed, _rigidbody.velocity.y);
-        else
-            _rigidbody.velocity = new Vector2(horizontal_direction * _player_speed/2, _rigidbody.velocity.y);
+        CheckGround();
+        SlopeCheck();
+        ApplyMovement();
+    }
+
+    private void CheckInput()
+    {
+        xInput = Input.GetAxisRaw("Horizontal");
+
+        if(xInput != 0)
+            Flip();
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            Jump();
+        }
+
+    }
+    private void CheckGround()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
+
+        if (rb.velocity.y <= 0.0f)
+        {
+            isJumping = false;
+        }
+
+        if (isGrounded && !isJumping && slopeDownAngle <= maxSlopeAngle)
+        {
+            canJump = true;
+        }
 
     }
 
-    void CheckPlayerInput()
+    private void SlopeCheck()
     {
-        horizontal_direction = Input.GetAxisRaw("Horizontal");
+        Vector2 checkPos = transform.position - (Vector3)(new Vector2(0.0f, capsuleColliderSize.y / 2));
 
-        if (Input.GetButtonDown("Jump") && _is_grounded)
+        SlopeCheckHorizontal(checkPos);
+        SlopeCheckVertical(checkPos);
+    }
+
+    private void SlopeCheckHorizontal(Vector2 checkPos)
+    {
+        RaycastHit2D slopeHitFront = Physics2D.Raycast(checkPos, transform.right, slopeCheckDistance, whatIsGround);
+        RaycastHit2D slopeHitBack = Physics2D.Raycast(checkPos, -transform.right, slopeCheckDistance, whatIsGround);
+
+        if (slopeHitFront)
         {
-            _rigidbody.AddForce(new Vector2(0, _jump_force), ForceMode2D.Impulse);
+            isOnSlope = true;
+
+            slopeSideAngle = Vector2.Angle(slopeHitFront.normal, Vector2.up);
+
+        }
+        else if (slopeHitBack)
+        {
+            isOnSlope = true;
+
+            slopeSideAngle = Vector2.Angle(slopeHitBack.normal, Vector2.up);
+        }
+        else
+        {
+            slopeSideAngle = 0.0f;
+            isOnSlope = false;
+        }
+
+    }
+
+    private void SlopeCheckVertical(Vector2 checkPos)
+    {
+        RaycastHit2D hit = Physics2D.Raycast(checkPos, Vector2.down, slopeCheckDistance, whatIsGround);
+
+        if (hit)
+        {
+
+            slopeNormalPerp = Vector2.Perpendicular(hit.normal).normalized;
+
+            slopeDownAngle = Vector2.Angle(hit.normal, Vector2.up);
+
+            if (slopeDownAngle != lastSlopeAngle)
+            {
+                isOnSlope = true;
+            }
+
+            lastSlopeAngle = slopeDownAngle;
+
+            Debug.DrawRay(hit.point, slopeNormalPerp, Color.blue);
+            Debug.DrawRay(hit.point, hit.normal, Color.green);
+
+        }
+
+        if (slopeDownAngle > maxSlopeAngle || slopeSideAngle > maxSlopeAngle)
+        {
+            canWalkOnSlope = false;
+        }
+        else
+        {
+            canWalkOnSlope = true;
+        }
+
+        if (isOnSlope && canWalkOnSlope && xInput == 0.0f)
+        {
+            rb.sharedMaterial = fullFriction;
+        }
+        else
+        {
+            rb.sharedMaterial = noFriction;
         }
     }
 
-    void UpdateAnimatorKeys()
+    private void Jump()
     {
-        bool is_running = horizontal_direction != 0;
-        _animator.SetBool(is_running_key, is_running);
-
-        _animator.SetBool(is_grounded_key, _is_grounded);
-        _animator.SetFloat(y_velocity_key, _rigidbody.velocity.y);
+        if (canJump)
+        {
+            canJump = false;
+            isJumping = true;
+            newVelocity.Set(0.0f, 0.0f);
+            rb.velocity = newVelocity;
+            newForce.Set(0.0f, jumpForce);
+            rb.AddForce(newForce, ForceMode2D.Impulse);
+        }
     }
 
-    void FlipCharacter()
+    private void ApplyMovement()
     {
-        if(horizontal_direction < 0)
+        if (isGrounded && !isOnSlope && !isJumping) //if not on slope
+        {
+            Debug.Log("This one");
+            newVelocity.Set(movementSpeed * xInput, 0.0f);
+            rb.velocity = newVelocity;
+        }
+        else if (isGrounded && isOnSlope && canWalkOnSlope && !isJumping) //If on slope
+        {
+            newVelocity.Set(movementSpeed * slopeNormalPerp.x * -xInput, movementSpeed * slopeNormalPerp.y * -xInput);
+            rb.velocity = newVelocity;
+        }
+        else if (!isGrounded) //If in air
+        {
+            newVelocity.Set(movementSpeed * xInput, rb.velocity.y);
+            rb.velocity = newVelocity;
+        }
+
+    }
+
+    private void Flip()
+    {
+        if (xInput < 0)
         {
             _sprite_renderer.flipX = true;
         }
-        else if(horizontal_direction > 0)
+        else if (xInput > 0)
         {
             _sprite_renderer.flipX = false;
         }
     }
 
-    void OnTriggerStay2D(Collider2D otherCollider)
+    private void OnDrawGizmos()
     {
-        if (otherCollider.gameObject.GetComponent<Platform>())
-        {
-            _is_grounded = true;
-            //_sprite_renderer.color = Color.green;
-        }
+        Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);
     }
-    void OnTriggerExit2D(Collider2D otherCollider)
-    {
-        if (otherCollider.gameObject.GetComponent<Platform>())
-        {
-            _is_grounded = false;
-           // _sprite_renderer.color = Color.white;
-        }
-    }
+
 }
