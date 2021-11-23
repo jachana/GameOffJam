649f19e290741f2d76bc52eb568a984715be7305
Víctor Adrián Suarez Ruiz <jr._5oct1998@hotmail.com>
2021-11-23 15:19:26 -0600

    Fix player bumping

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -34,11 +34,7 @@ public class PlayerController : MonoBehaviour
 
     private void FixedUpdate()
     {
-        if (_is_grounded)
-            _rigidbody.velocity = new Vector2(horizontal_direction * _player_speed, _rigidbody.velocity.y);
-        else
-            _rigidbody.velocity = new Vector2(horizontal_direction * _player_speed/2, _rigidbody.velocity.y);
-
+        _rigidbody.velocity = new Vector2(horizontal_direction * _player_speed, _rigidbody.velocity.y);
     }
 
     void CheckPlayerInput()
